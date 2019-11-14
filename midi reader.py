4e673b5d18180f76acdb7d7db1b870d815843909
2020dcha <49345437+2020dcha@users.noreply.github.com>
2019-11-14 12:09:48 -0500

    Add files via upload

---
+++ midi reader.py
@@ -0,0 +1,20 @@
+from mido import MidiFile
+
+mid = MidiFile('Mii Channel.mid')
+file = open("data.txt", "w")
+min=100
+max=0
+
+for i, track in enumerate(mid.tracks):
+    for msg in track:
+        if str(msg)[:4]=="note":
+            file.write(str(msg))
+            file.write("\n")
+            temp=str(msg).index("note=")
+            note=int(str(msg)[temp+5:temp+7])
+            if min>note:
+                min=note
+            if max<note:
+                max=note
+file.close()
+print(max, min, max-min)
