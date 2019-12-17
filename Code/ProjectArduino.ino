40e6443a8cfeab9821fad338ab2b06a44e1d145a
2020dcha <49345437+2020dcha@users.noreply.github.com>
2019-12-17 10:38:43 -0500

    Update ProjectArduino.ino

--- Code/ProjectArduino.ino
+++ Code/ProjectArduino.ino
@@ -1,15 +1,28 @@
-#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
 #define servoPin 2
+#define SERVOMIN2  105 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX2  120 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN4  110 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX4  120
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+// our servo # counter
+uint8_t servonum = 1;
+int servospeed = 2;
 int temp=0;
 
 //https://maker.pro/arduino/tutorial/how-to-make-arduino-and-processing-ide-communicate
-Servo Servo1;
 
 void setup() { 
-Servo1.attach(servoPin);
-Servo1.write(0);
-Serial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
+
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);  // The int.osc. is closer to 27MHz  
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+
+  delay(10);
 }
 
 void loop() {
@@ -37,3 +50,43 @@ void loop() {
   digitalWrite(temp, LOW); 
  }
  }
+/*
+
+void loop() {
+  // Drive each servo one at a time using setPWM()
+  servonum = 1;
+  
+  //if(servospeed > 6)
+  //servospeed = 2;
+  
+  Serial.println(servonum);
+  for (uint16_t pulselen = SERVOMAX2; pulselen > SERVOMIN2; pulselen--) {
+    pwm.setPWM(servonum, 0, pulselen);
+    delay(servospeed);
+  }
+
+  for (uint16_t pulselen = SERVOMIN2; pulselen < SERVOMAX2; pulselen++) {
+    pwm.setPWM(servonum, 0, pulselen);
+    delay(servospeed);
+  }
+
+  servonum = 3;
+
+  Serial.println(servonum);
+
+  delay(500);
+  for (uint16_t pulselen = SERVOMAX4; pulselen > SERVOMIN4; pulselen--) {
+    pwm.setPWM(servonum, 0, pulselen);
+    delay(servospeed);
+  }
+
+  Serial.println(servonum);
+  for (uint16_t pulselen = SERVOMIN4; pulselen < SERVOMAX4; pulselen++) {
+    pwm.setPWM(servonum, 0, pulselen);
+    delay(servospeed);
+  }
+
+  delay(500);
+
+  //servospeed += 1;
+}*/
